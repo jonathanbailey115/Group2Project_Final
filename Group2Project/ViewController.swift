5ff419100f6af6fc47607e2d4d492d346ced1dd6
Jonathan Bailey <jonathan_bailey115@mymail.eku.edu>
2025-11-18 22:00:10 -0500

    Initial Commit

---
+++ Group2Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group2Project
+//
+//  Created by Bailey, Jonathan D. on 11/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
